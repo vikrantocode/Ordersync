62b5652326a797f6a220a9833b28a488443fd1a8
vikrantocode <vikrantk@ocodewire.com>
2021-11-18 15:11:43 +0530

    add process-sync process lock

--- controllers/ordersync.js
+++ controllers/ordersync.js
@@ -1,6 +1,7 @@
 const uploads = './uploads';
 const uploaded = './uploaded';
 var path = require('path')
+var ps = require('process-sync')
 const csv = require('csvtojson');
 const _ = require('lodash-contrib');
 const xlsx = require('xlsx')
@@ -19,7 +20,7 @@ const moveFile =function (oldPath, newPath) {
 		    if (err) {
 				console.log(err, "--------------------error")
 			}
-	})  
+	}) 
 }
 
 //? Helper Function to Rename Keys
@@ -118,177 +119,182 @@ const renameToDesiredFormat = (data) => {
 	return result;
 };
 
-const UploadOrder=async function(orders,oldPath, newPath ){
-		const t =await sequelize.transaction();
-		try {
-			for (data of orders) {
-				const orderDetails = _.pick(data, [
-					'customerId',
-					'orderSource',
-					'orderSourceOrderId',
-					'isRushOrder',
-					'packageType',
-					'deliveryDate',
-					'locationNotes',
-					'eBaySalesRecordNumber',
-					'serialNumber',
-					'trackingNumber',
-					'disputeStartedOn',
-					'isInDispute',
-					'siteCode',
-					'googleOrderNumber',
-					'customerServiceStatus',
-					'invoicePrinted',
-					'invoicePrintedDate',
-					'status',
-					'timeOfOrder'
-				]);
-				const paymentDetails = _.pick(data, [
-					'paymentStatus',
-					'payementDate',
-					'paymentReferenceNumber',
-					'paymentMethod',
-					'orderCurrency',
-					'orderDiscountsTotal',
-					'insuranceTotal',
-					'subTotal',
-					'grandTotal',
-					'taxRate',
-					'taxTotal',
-					'lineTotal',
-					'finalValueTotal',
-					'postingFeeTotal',
-					'payPalFeeTotal',
-					'orderSourceTotal',
-					'orderId'
-				]);
-				const shippingDetails = _.pick(data, [
-					'shippingTotal',
-					'shippingDiscountsTotal',
-					'dropShipFeeTotal',
-					'shippingDate',
-					'shipFirstName',
-					'shippingLastName',
-					'shipCompanyName',
-					'shipAddress1',
-					'shipAddress2',
-					'shipCity',
-					'shipState',
-					'shipZipCode',
-					'shipCountry',
-					'shipPhoneNumber',
-					'shippingMethodSelected',
-					'companyId',
-					'shippingCarrier',
-					'shippedBy',
-					'shipFromWarehouse',
-					'shippingFee',
-					'originalShippingCost',
-					'adjustedShippingCost',
-					'shippingWeightTotalOz',
-					'shippingStatus',
-					'stationId',
-					'orderId'
-				]);
-				const billingDetails = _.pick(data, [
-					'billingTotal',
-					'billingDiscountsTotal',
-					'billingDate',
-					'billingFirstName',
-					'billingLastName',
-					'billingCompanyName',
-					'billingAddress1',
-					'billingAddress2',
-					'billingCity',
-					'billingState',
-					'billingZipCode',
-					'billingCountry',
-					'billingPhoneNumber',
-					'billingMethodSelected'
-				]);
-				const orderItemDetails = _.pick(data, [
-					'adjustedUnitPrice',
-					'originalUnitPrice',
-					'handlingFee',
-					'giftWrapCharge',
-					'qty',
-					'backOrderQty',
-					'orderId'
-				]);
-				const { id } = await Order.create(orderDetails, { transaction: t });
-				paymentDetails.orderId = id
-				shippingDetails.orderId = id
-				billingDetails.orderId = id
-				orderItemDetails.orderId = id
-				await PaymentDetail.create(paymentDetails, { transaction: t });
-				await BillingDetail.create(billingDetails, { transaction: t });
-				await ShippingDetail.create(shippingDetails, { transaction: t });
-				await ShippingDetail.create(shippingDetails, { transaction: t });
-				await OrderItem.create(orderItemDetails, { transaction: t });
-				}	// End for 
 
-			t.commit();
-			
-			//  Move file to uploded folder after successfully imported 
-			moveFile(oldPath, newPath)
-			return 1;
-			
-			} catch (err) {
-				t.rollback();
-				return err;
-		}		
+const readfile = async (file)=>{
+	let filext = path.extname(file)
+	if (filext ==='.csv'){
+		const jsonOrders =await csv().fromFile(file);
+		const orders = renameToDesiredFormat(jsonOrders);
+		console.log(orders, "---------------------------csv")
+		return orders;
+	}
+	else if (filext ==='.xlsx'){
+		const wb = xlsx.readFile(file)
+		const ws = wb.Sheets['sample']
+		const datas = xlsx.utils.sheet_to_json(ws)
+		console.log(datas, "---------------------------xlsx")
+		return datas;
 	}
 
-const Ordersync = (req, res) => {
+}
+
+const UploadOrder=function(file, oldPath, newPath ){
+
+	ps.lock();
+	setTimeout(function(){
+		readfile(oldPath).then(async(order) =>{
+			const t =await sequelize.transaction();
+			let date_ob1 = new Date();
+			// current hours
+			let hours1 = date_ob1.getHours();
+			// current minutes
+			let minutes1 = date_ob1.getMinutes();
+			// current seconds
+			let seconds1 = date_ob1.getSeconds();
+			try {
+				for (data of order) {
+					const orderDetails = _.pick(data, [
+						'customerId',
+						'orderSource',
+						'orderSourceOrderId',
+						'isRushOrder',
+						'packageType',
+						'deliveryDate',
+						'locationNotes',
+						'eBaySalesRecordNumber',
+						'serialNumber',
+						'trackingNumber',
+						'disputeStartedOn',
+						'isInDispute',
+						'siteCode',
+						'googleOrderNumber',
+						'customerServiceStatus',
+						'invoicePrinted',
+						'invoicePrintedDate',
+						'status',
+						'timeOfOrder'
+					]);
+					const paymentDetails = _.pick(data, [
+						'paymentStatus',
+						'payementDate',
+						'paymentReferenceNumber',
+						'paymentMethod',
+						'orderCurrency',
+						'orderDiscountsTotal',
+						'insuranceTotal',
+						'subTotal',
+						'grandTotal',
+						'taxRate',
+						'taxTotal',
+						'lineTotal',
+						'finalValueTotal',
+						'postingFeeTotal',
+						'payPalFeeTotal',
+						'orderSourceTotal',
+						'orderId'
+					]);
+					const shippingDetails = _.pick(data, [
+						'shippingTotal',
+						'shippingDiscountsTotal',
+						'dropShipFeeTotal',
+						'shippingDate',
+						'shipFirstName',
+						'shippingLastName',
+						'shipCompanyName',
+						'shipAddress1',
+						'shipAddress2',
+						'shipCity',
+						'shipState',
+						'shipZipCode',
+						'shipCountry',
+						'shipPhoneNumber',
+						'shippingMethodSelected',
+						'companyId',
+						'shippingCarrier',
+						'shippedBy',
+						'shipFromWarehouse',
+						'shippingFee',
+						'originalShippingCost',
+						'adjustedShippingCost',
+						'shippingWeightTotalOz',
+						'shippingStatus',
+						'stationId',
+						'orderId'
+					]);
+					const billingDetails = _.pick(data, [
+						'billingTotal',
+						'billingDiscountsTotal',
+						'billingDate',
+						'billingFirstName',
+						'billingLastName',
+						'billingCompanyName',
+						'billingAddress1',
+						'billingAddress2',
+						'billingCity',
+						'billingState',
+						'billingZipCode',
+						'billingCountry',
+						'billingPhoneNumber',
+						'billingMethodSelected'
+					]);
+					const orderItemDetails = _.pick(data, [
+						'adjustedUnitPrice',
+						'originalUnitPrice',
+						'handlingFee',
+						'giftWrapCharge',
+						'qty',
+						'backOrderQty',
+						'orderId'
+					]);
+					const { id } = await Order.create(orderDetails, { transaction: t });
+					// paymentDetails.orderId = id
+					// shippingDetails.orderId = id
+					// billingDetails.orderId = id
+					// orderItemDetails.orderId = id
+					// await PaymentDetail.create(paymentDetails, { transaction: t });
+					// await BillingDetail.create(billingDetails, { transaction: t });
+					// await ShippingDetail.create(shippingDetails, { transaction: t });
+					// await ShippingDetail.create(shippingDetails, { transaction: t });
+					// await OrderItem.create(orderItemDetails, { transaction: t });
+					console.log(id)
+					}	// End for 
+
+				t.commit();
+
+				
+				// //  Move file to uploded folder after successfully imported 
+				// moveFile(oldPath, newPath)
+				
+				let date_ob2 = new Date();
+					// current hours
+				let hours2 = date_ob2.getHours();
+				// current minutes
+				let minutes2 = date_ob2.getMinutes();
+				// current seconds
+				let seconds2 = date_ob2.getSeconds();
+				console.log("-------------------------------",file," Upload time = ",(hours2-hours1) + ":" + (minutes2-minutes1)+ ":" + (seconds2- seconds1))
+				ps.unlock();
+			} catch (err) {
+					console.log(err, "------------------------------------------------------err")
+					t.rollback();
+					ps.unlock();
+
+			}	
+
+		})
+	},3000);
+
+	}
+
+const Ordersync = async(req, res) => {
 	// Read file inside Uplods dir 
 	 fs.readdir(uploads,(err, files) => {	   
 		files.forEach(async(file) => {
+
 			const oldPath = (uploads+"/"+file)
 			const newPath = (uploaded+"/"+file)
-			let filext = path.extname(oldPath)
-			if (filext ==='.csv'){
-				const jsonOrders =await csv().fromFile(oldPath);
-				var orders = renameToDesiredFormat(jsonOrders);
-				console.log(orders, "---------------------------csv")
-			}
-			else if (filext ==='.xlsx'){
-				const wb = xlsx.readFile(oldPath,)
-				const ws = wb.Sheets['sample']
-				const jsonOrder = xlsx.utils.sheet_to_json(ws)
-				var orders = renameToDesiredFormat(jsonOrder);
-				console.log(orders, "---------------------------xlsx")
-			}
-			else{
-				console.log("Wrong file format")	
-			}
-				const status = UploadOrder(orders,oldPath, newPath)
-				let date_ob1 = new Date();
-				// current hours
-				let hours1 = date_ob1.getHours();
-	
-				// current minutes
-				let minutes1 = date_ob1.getMinutes();
-	
-				// current seconds
-				let seconds1 = date_ob1.getSeconds();
-				status.then(data => {
-					let date_ob2 = new Date();
-					// current hours
-					let hours2 = date_ob2.getHours();
-		
-					// current minutes
-					let minutes2 = date_ob2.getMinutes();
-		
-					// current seconds
-					let seconds2 = date_ob2.getSeconds();
-		
-					if (data == 1){
-						console.log("-------------------------------------------------------------------------",file," Upload time = ",(hours2-hours1) + ":" + (minutes2-minutes1)+ ":" + (seconds2- seconds1))
-					}else{
-						console.log(data, "---something went wrong")
-						}
-					}
-				)
+			ps.process(UploadOrder,[file,oldPath, newPath]);
+			
 			});
 	  	}); 
 	}
